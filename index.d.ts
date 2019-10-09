e39934bc797f20aed5e009918f36f8d981af9bd3
Piyush Mishra <semper@Piyushs-MacBook-Pro.local>
2019-10-10 00:02:19 +0530

    fix .gitignore to ignore compiled index.ts

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-export { DlnS3 } from "./lib/dln-s3";
-export { S3ACL } from "./lib/helpers/enums/s3-acl.enum";
-export { S3Error } from "./lib/helpers/enums/s3-error.enum";
-export { S3Operation } from "./lib/helpers/enums/s3-operation.enum";
-export { S3UrlKeyPosition } from "./lib/helpers/enums/s3-url-key-postition.enum";
-export { S3Credentials } from "./lib/helpers/s3-credentials";
-export { S3SignedUrlConfig } from "./lib/helpers/s3-signed-url-config";
-export { S3UploadConfig } from "./lib/helpers/s3-upload-config";
-export { S3UrlConfig } from "./lib/helpers/s3-url-config";
